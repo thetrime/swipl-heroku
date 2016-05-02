cbfe17f9b1798e7ce66b142c062531f7c5d5600f
Matt Lilley <thetrime@gmail.com>
2016-05-03 04:14:43 +1200

    More adjustments

--- compile.sh
+++ compile.sh
@@ -15,7 +15,7 @@ fi
 set -e
 BIN_DIR=$(cd $(dirname $0); pwd) # absolute path
 bpdir=$(cd $(dirname $(dirname $0)); pwd)
-mkdir -p build cache
+mkdir -p build cache .local
 build=$(cd "build/" && pwd)
 test -z ${build} && exit
 cache=$(cd "cache/" && pwd)
@@ -25,96 +25,67 @@ PROFILE=${HOME}/.profile.d
 # If we have a copy of the build environment already, just use that!
 echo "------> Compiling Prolog"
 (
-    # Testing: Recompile all
-    # rm ${cache}/*.tgz
-
     # First though, do we have unixodbc?
-    echo "------> Checking for unixODBC"
+    echo "------> unixODBC"
     (
-        test -f ${cache}/unixodbc.tgz && exit
         echo "------> Fetching unixODBC"
         mkdir -p ${build}/unixodbc-build
         cd ${build}/unixodbc-build
         curl ftp://ftp.unixodbc.org/pub/unixODBC/unixODBC-2.3.4.tar.gz -s -o - | tar -zx -f -
         cd unixODBC-2.3.4
         echo "------> Compiling unixODBC"
-        ./configure --prefix=/app/.odbc > /dev/null 2>&1
+        ./configure --prefix=/app/.local > /dev/null 2>&1
         make > /dev/null 2>&1
         make install > /dev/null 2>&1
         cd /app
-        tar -czf ${cache}/unixodbc.tgz .odbc
         rm -rf ${build}/unixodbc-build
-        rm -rf /app/.odbc
     )
-    echo "------> Unpacking unixODBC"
-    cd /app
-    tar -xzf ${cache}/unixodbc.tgz
-
     # Then: Do we have gmp?
-    echo "------> Checking for gmp"
+    echo "------> gmp"
     (
-        test -f ${cache}/gmp.tgz && exit
         echo "------> Fetching gmp"
         mkdir -p ${build}/gmp-build
         cd ${build}/gmp-build
         curl https://ftp.gnu.org/gnu/gmp/gmp-6.0.0a.tar.bz2 -s -o - | tar -jx -f -
         cd gmp-6.0.0
         echo "------> Compiling gmp"
-        ./configure --prefix=/app/.gmp > /dev/null 2>&1
+        ./configure --prefix=/app/.local > /dev/null 2>&1
         make > /dev/null 2>&1
         make install > /dev/null 2>&1
         cd /app
-        tar -czf ${cache}/gmp.tgz .gmp
         rm -rf ${build}/gmp-build
-        rm -rf /app/.gmp
     )
-    echo "------> Unpacking gmp"
-    cd /app
-    tar -zxf ${cache}/gmp.tgz
-
     # Next: Do we have psqlodbc?
-    echo "------> Checking for psqlodbc"
+    echo "------> psqlodbc"
     (
-        test -f ${cache}/psqlodbc.tgz && exit
         echo "------> Fetching psqlodbc"
         mkdir -p ${build}/psqlodbc-build
         cd ${build}/psqlodbc-build 
         curl http://ftp.postgresql.org/pub/odbc/versions/src/psqlodbc-09.01.0200.tar.gz -s -o - | tar -zx -f -
         cd psqlodbc-09.01.0200
         echo "------> Compiling psqlodbc"
-        LD_LIBRARY_PATH=/app/.odbc/lib ./configure --prefix=/app/.psqlodbc --with-unixodbc=/app/.odbc --without-libpq > /dev/null 2>&1
+        LD_LIBRARY_PATH=/app/.local/lib ./configure --prefix=/app/.local --with-unixodbc=/app/.local --without-libpq > /dev/null 2>&1
         make > /dev/null 2>&1
         make install > /dev/null 2>&1
         cd /app
-        tar -czf ${cache}/psqlodbc.tgz .psqlodbc
         rm -rf ${build}/psqlodbc-build
-        rm -rf /app/.psqlodbc
     )
-    echo "------> Unpacking psqlodbc"
-    cd /app
-    tar -xzf ${cache}/psqlodbc.tgz
 
     # Next: Do we have libarchive?
-    echo "------> Checking for libarchive"
+    echo "------> libarchive"
     (
-        test -f ${cache}/libarchive.tgz && exit
         echo "------> Fetching libarchive"
         mkdir -p ${build}/libarchive-build
         cd ${build}/libarchive-build
         curl http://www.libarchive.org/downloads/libarchive-3.2.0.tar.gz -s -o - | tar -zx -f -
         cd libarchive-3.2.0
         echo "------> Compiling libarchive"
-        ./configure --prefix=/app/.libarchive > /dev/null 2>&1
+        ./configure --prefix=/app/.local > /dev/null 2>&1
         make > /dev/null 2>&1
         make install > /dev/null 2>&1
         cd /app
-        tar -czf ${cache}/libarchive.tgz .libarchive
         rm -rf ${build}/libarchive-build
-        rm -rf /app/.libarchive
     )
-    echo "------> Unpacking libarchive"
-    cd /app
-    tar -xzf ${cache}/libarchive.tgz
 
     # Finally: Prolog
     echo "-----> Building prolog"
@@ -144,23 +115,19 @@ echo "------> Compiling Prolog"
         # Prepare the build
         ./prepare --yes --all --man > /dev/null 2>&1
         # Configure and build it
-        export LD_RUN_PATH=/app/.odbc/lib
-        export LDFLAGS="$LDFLAGS -LLIBDIR -Wl,-rpath=/app/.odbc/lib -L/app/.gmp/lib -Wl,-rpath=/app/.gmp/lib"
-        export CFLAGS="$CFLAGS -I/app/.gmp/include"
-        ./configure --with-world --with-odbc=/app/.odbc --prefix=/app/.swipl> /dev/null 2>&1
+        export LD_RUN_PATH=/app/.local/lib
+        export LDFLAGS="$LDFLAGS -LLIBDIR -Wl,-rpath=/app/.local/lib -L/app/.local/lib -Wl,-rpath=/app/.local/lib"
+        export CFLAGS="$CFLAGS -I/app/.local/include"
+        ./configure --with-world --with-odbc=/app/.local --prefix=/app/.local> /dev/null 2>&1
         make > /dev/null 2>&1
         echo "------> Installing SWI Prolog ${ID}"
         make install > /dev/null 2>&1
         cd /app
-        tar -czf ${cache}/swipl-${ID}.tgz .swipl
         # Now clean up
         rm -rf ${build}/swipl-build
-        rm -rf /app/.swipl
         echo "------> Installed SWI Prolog ${ID}"
-        cd /app
-        tar -xzf ${cache}/swipl-${ID}.tgz
-        tar -cjf ${BIN_DIR}/env-${ID}.tar.bz2 /app
-
+        tar -cjf ${BIN_DIR}/env-${ID}.tar.bz2 .local
+        rm -rf .local
         echo "Environment is in ${BIN_DIR}/env-${ID}.tar.bz2"
 
     )
